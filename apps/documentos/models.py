5ae272128eb49d20dc6ea1530e7982eae27a4f9c
jesielcarlos <jesielcarlos17@gmail.com>
2021-05-27 21:03:20 -0300

    Update documentos

--- apps/documentos/models.py
+++ apps/documentos/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from apps.funcionarios.models import Funcionario
+from django.shortcuts import reverse
 
 
 class Documento(models.Model):
@@ -7,6 +8,8 @@ class Documento(models.Model):
     pertence = models.ForeignKey(Funcionario,on_delete=models.PROTECT)
     arquivo = models.FileField(upload_to='documentos')
 
+    def get_absolute_url(self):
+        return reverse('update_funcionario', args=[self.pertence.id])
 
     def __str__(self):
         return self.descricao
---
+++ apps/documentos/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import DocumentoCreate
+
+
+urlpatterns = [
+    path('novo/<int:funcionario_id>/', DocumentoCreate.as_view(),name='create_documento'),
+    #path('deletar/<int:pk>/',DocumentoDelete.as_view(),name='delete_documento'),
+]
+
+
